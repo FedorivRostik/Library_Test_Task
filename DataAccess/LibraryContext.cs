efc6847db88ab6bff184c5d1359a21f10202db8a
Rostyslav Fedoriv <froizerner@gmail.com>
2023-02-16 11:25:11 +0200

    Сonfigured project structure

---
+++ DataAccess/LibraryContext.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+public class LibraryContext : DbContext
+{
+}
--- Library_Test_Task/Program.cs
+++ Library_Test_Task/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//builder.Services.AddDbContext<DatabaseName>(o=> o.UseInMemoryDatabase("Librarydb"))
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
