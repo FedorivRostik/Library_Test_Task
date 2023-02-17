74009f13e5e4b759e2108f78cf617c15d78039c8
Rostyslav Fedoriv <froizerner@gmail.com>
2023-02-17 23:34:09 +0200

    Сreated configuration for entities

---
+++ DataAccess/Configurations/BookConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Core.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Configurations;
+public class BookConfiguration : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder
+            .HasKey(b => b.Id);
+
+        builder
+            .HasIndex(b => b.Id);
+
+        builder
+            .Property(b => b.Title)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder
+            .Property(b => b.Content)
+            .HasMaxLength(5000)
+            .IsRequired();
+
+        builder
+            .Property(b => b.Author)
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder
+            .Property(b => b.Cover);
+
+        builder
+            .Property(b => b.Genre)
+            .HasMaxLength(20);
+    }
+}
---
+++ DataAccess/Configurations/RatingConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Configurations;
+internal class RatingConfiguration : IEntityTypeConfiguration<Rating>
+{
+    public void Configure(EntityTypeBuilder<Rating> builder)
+    {
+        builder
+          .HasKey(b => b.Id);
+
+        builder
+            .HasIndex(b => b.Id);
+
+        builder.Property(b => b.Score)
+            .IsRequired();
+
+
+        builder.HasOne(b => b.Book)
+            .WithMany(b => b.Ratings)
+            .HasForeignKey(b => b.Book.Id)
+            .OnDelete(DeleteBehavior.Cascade);
+
+
+    }
+}
